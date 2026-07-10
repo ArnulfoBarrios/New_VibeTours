95c7fae553668be4503200d9af5e0d93ddbe3e9f
ArnulfoBarrios <abarrios62@cuc.edu.co>
2026-07-10 08:44:46 -0500

    feat: configure custom project build directory and suppress Java compilation warnings

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -17,6 +17,17 @@ subprojects {
 }
 subprojects {
     project.evaluationDependsOn(":app")
+    
+    // Suprimir avisos molestos de plugins de terceros
+    tasks.withType<JavaCompile>().configureEach {
+        options.compilerArgs.addAll(listOf(
+            "-Xlint:-options",      // Suprime 'source value 8 is obsolete'
+            "-Xlint:-unchecked",    // Suprime 'unchecked or unsafe operations'
+            "-Xlint:-deprecation",  // Suprime 'override a deprecated API'
+            "-nowarn",              // Suprime todos los warnings restantes
+            "-Xlint:none"           // Otra forma de forzar la supresión
+        ))
+    }
 }
 
 tasks.register<Delete>("clean") {
